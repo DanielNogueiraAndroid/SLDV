add0a0b7041c3a128afa565f410c25223c19e7ae
Daniel Nogueira <dnogueira86@gmail.com>
2018-12-15 17:03:15 +0000

    map sensor to analogWrite on vibration motor

--- sketch_try_to_read_sonaranalogfeather/sketch_try_to_read_sonaranalogfeather.ino
+++ sketch_try_to_read_sonaranalogfeather/sketch_try_to_read_sonaranalogfeather.ino
@@ -1,6 +1,6 @@
 const int anPin = A5;
 const int vMotor = 11;
-long anVolt, mm, inches;
+long anVolt, mm, inches, pwm;
 
 void setup() {
 Serial.begin(9600);
@@ -12,12 +12,16 @@ void read_sensor(){
 anVolt = analogRead(anPin);
 mm = anVolt * 5;
 inches = mm/25.4;
+pwm = map(mm, 0,2500,200,50);
 }
 
 void print_range(){
 Serial.print("S1");
 Serial.print("=");
 Serial.print(mm);
+Serial.print("pwm");
+Serial.print("=");
+Serial.print(pwm);
 Serial.print(" ");
 Serial.println(inches);
 }
@@ -25,9 +29,9 @@ Serial.println(inches);
 void loop() {
 read_sensor();
 print_range();
-analogWrite(vMotor, 52);
+analogWrite(vMotor, pwm);
 //delay(20-(mm/200));
-//delay(100);
+delay(10);
 //digitalWrite(vMotor, LOW);
 //delay(100);
 
